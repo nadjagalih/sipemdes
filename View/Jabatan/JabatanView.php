ad92ef0738765229aefd8ac2c7e58abea0397eb5
Devin Rayhan Putra Aswin <devin.23351@mhs.unesa.ac.id>
2025-09-16 14:43:49 +0700

    Fix bagian upload file SK mutasi di Pegawai detail

--- View/Jabatan/JabatanView.php
+++ View/Jabatan/JabatanView.php
@@ -76,23 +76,7 @@ if ($_GET['alert'] == 'Edit') {
             <div class="ibox ">
                 <div class="ibox-title">
                     <h5>List Data Jabatan</h5> <a href="?pg=JabatanAdd" class="btn btn-primary float-center"> Add Jabatan</a>
-                    <div class="ibox-tools">
-                        <a class="collapse-link">
-                            <i class="fa fa-chevron-up"></i>
-                        </a>
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="fa fa-wrench"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-user">
-                            <li><a href="#" class="dropdown-item">Config option 1</a>
-                            </li>
-                            <li><a href="#" class="dropdown-item">Config option 2</a>
-                            </li>
-                        </ul>
-                        <a class="close-link">
-                            <i class="fa fa-times"></i>
-                        </a>
-                    </div>
+
                 </div>
 
                 <div class="ibox-content">
